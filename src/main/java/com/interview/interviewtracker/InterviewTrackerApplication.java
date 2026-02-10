83ea205b014c67f498a1d93014e24ca1868d8f97
Bibhu20031 <bibhusmaran@gmail.com>
2026-02-10 20:42:38 +0530

    Initial app setup with health check

---
+++ src/main/java/com/interview/interviewtracker/InterviewTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.interview.interviewtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewTrackerApplication.class, args);
+	}
+
+}
+
